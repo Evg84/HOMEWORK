0405b88a3ee3e5ef0bd321e4614f32ef98f475f0
Evgenya <zhenya.egorova.84@bk.ru>
2022-10-16 11:57:52 +0700

    number15

---
+++ number15/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Введите день недели цифрой :");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (1 <= n && n <= 5)
+{
+    Console.WriteLine("Этот день рабочий");
+}
+else if (6 <= n && n <= 7)
+{
+    Console.WriteLine("Этот день выходной");
+}
+else 
+{
+    Console.WriteLine("Такого дня недели не существует");
+}
