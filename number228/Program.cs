3bd0a26b3bf7aed2d4cc89f5a29fbeb04efa2890
Evgenya <zhenya.egorova.84@bk.ru>
2022-10-16 12:03:36 +0700

    number228

---
+++ number228/Program.cs
@@ -0,0 +1,15 @@
+﻿int i = 1;
+float total = 0;
+int count = 0;
+
+while (i < 8)
+{
+    Console.WriteLine("Введите число №" + i +" ");
+    count = Convert.ToInt32(Console.ReadLine());
+    total = total + count;
+    i++;
+}
+
+total = total / 7;
+Console.WriteLine("Среднее арифметическое равно :");
+Console.WriteLine(total);
